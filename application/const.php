e434b3658074c0e239c5719ed3b02905abc05a99
komiles <komiles@163.com>
2015-08-31 16:55:13 +0800

    *update

--- application/const.php
+++ application/const.php
@@ -1,3 +1,7 @@
 <?php
 define('FUNC_PATH',APP_PATH "/application/library/Function");
+$file_data = date('Y-m-d',time());
+define('EXCEPTION_FILE_NAME',$file_data.".log");
+define('TB_PK',        'id');
+
 
