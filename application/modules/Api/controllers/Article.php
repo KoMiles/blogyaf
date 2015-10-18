a6b34c286e6e2c010ed791c54973bf01c6ea2d6f
komiles <komiles@163.com>
2015-10-18 22:09:00 +0800

    *update api article

--- application/modules/Api/controllers/Article.php
+++ application/modules/Api/controllers/Article.php
@@ -24,34 +24,17 @@ class ArticleController extends Yaf_Controller_Abstract {
     public function getListAction() {
         $page = $this->getRequest()->getQuery("page") ? $this->getRequest()->getQuery("page") : 1;
         $limit = $this->getRequest()->getQuery("limit") ? $this->getRequest()->getQuery("limit") : 10;
+
+        $total_num = $this->m_article -> getArticleTotal('normal');
         $article_list = $this->m_article->getArticlesList($page, $limit, 'normal');
-        $total_num = $this->m_article -> getArticleTotal('normal');
         $result['list'] = $article_list;
-        $result['page'] = $total_num;
+        $pager = array(
+            'page' => $page,
+            'limit' => $limit,
+            'total_num' => $total_num,
+        );
 
-        echo Tool_String::jsonView('success',$result);
-        exit;
-    }
-    public function indexAction() {
-        $pageSize = 10;
-        $page = $this->getRequest()->getQuery("page") ? $this->getRequest()->getQuery("page") : 1;
-        //$page = $this->getRequest()->getParam("page") ? $this->getRequest()->getParam("page") : 1;
-        $article_list = $this->m_article->getArticlesList($page, $pageSize, 'normal');
-        foreach ($article_list as $key => $row) {
-            $article_list[$key]['date'] = date('Y-m-d H:i:s',$row['create_ts']);
-            $article_list[$key]['statusCn'] = $row['status'] == 'normal' ? '正常' : '已删除';
-        }
-        $total_num = $this->m_article -> getArticleTotal('normal');
-
-        $pageObj = new Tool_Pagination();
-        $page_html = $pageObj->markPhpPager('?page={page}',$page,$pageSize,$total_num);
-        //$page_html = $pageObj->markPhpPager('{page}',$page,$pageSize,$total_num);
-
-        $title = "文章列表";
-        $this->getView()->assign('page_string', $page_html);
-        $this->getView()->assign('title', $title);
-        $this->getView()->assign('article_list', $article_list);
-        $this->getView()->display('index.html');
+        echo Tool_String::jsonView('success', $result, '', $pager);
         exit;
     }
 }
