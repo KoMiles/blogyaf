7f56aa143b63894c632f99f986a71f1cf2ff7163
komiles <komiles@163.com>
2016-01-10 01:24:05 +0800

    *yaf捕获异常

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -8,10 +8,14 @@
  * @version $Id$
  * @author wangkongming <komiles@163.com> 
  */
-class ErrorController extends Yaf_Controller_Abstract {
+class ErrorController extends Yaf\Controller_Abstract {
 
-      public function errorAction($exception) {
-          $this->getView()->assign(“code”, $exception->getCode());
-          $this->getView()->assign(“message”, $exception->getMessage());
-      }
-   }
+    public function errorAction($exception) {
+        $message = $exception->__toString();
+        $this->getView()->assign("code", $exception->getCode());
+        //$this->getView()->assign("message", $exception->getMessage());
+        $this->getView()->assign("message", $message);
+        $this->getView()->display('error/error.html');
+        exit;
+    }
+}
